1069cce5ad7980ec4bb2123f54a7688c273742de
amalbel <belaidamal21@gmail.com>
2022-10-01 10:44:44 +0100

    onclick methode 2

--- app/src/main/java/android/example/hellotoast/MainActivity.java
+++ app/src/main/java/android/example/hellotoast/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +12,19 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount=0;
     private TextView mShowCount;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        Button btnCount=findViewById(R.id.count);
+    btnCount.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            countUp();
+
+        }
+    });
     }
 
     public void showToast(View view) {
@@ -25,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void countUp(View view) {
+    public void countUp() {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
